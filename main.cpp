42efd87a98f6fb9f72115cfb2630b04b5c779e8a
Kelukin Lau <Kelukin@gmail.com>
2021-02-15 02:23:02 +0000

    update

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@ void print_usage(){
 //    printf("[5]indexType  [6]lastDeleteType [7]indexThreshold\n");
     printf("[5]TrainFlag [6]indexType [7]timeThreshold\n");
     printf("[8]specialTrainSwitch[9]autoLogChooseFlag");
-    printf("\talg: greedy, NearLinear, PayAndTry, exact\n");
+    printf("\talg: greedy, NearLinear, PayAndRecycle, exact\n");
     printf("\tindexType: 0-GreedyIndex, 1-ProfitPriority, 2-Edge Estimation\n");
 //    printf("\tlastDeleteType: 0-basic, 1-smart");
 }
@@ -50,7 +50,7 @@ int main(int argc, char *argv[]) {
     graph->read_graph();// read the graph, including of the given set of vertices
     if(strcmp(argv[1], "greedy") == 0)  graph->greedy(k);
     else if(strcmp(argv[1], "n") == 0 || strcmp(argv[1], "NearLinear") == 0) graph->near_maximum_near_linear(k);
-    else if(strcmp(argv[1], "p") == 0 ||strcmp(argv[1], "PayAndTry") == 0) graph->pay_and_try_dominate_max_degree_greedy_delete_edges(k);
+    else if(strcmp(argv[1], "p") == 0 ||strcmp(argv[1], "PayAndRecycle") == 0) graph->pay_and_try_dominate_max_degree_greedy_delete_edges(k);
     else if(strcmp(argv[1], "exact") == 0) graph->exact_solver(k);
     //    std::cout << "Hello, World!" << std::endl;
     return 0;
