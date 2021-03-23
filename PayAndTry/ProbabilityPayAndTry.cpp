a24d4d1e04cec6e6b6e557bf58c21de19b81fed6
Kelukin Lau <Kelukin@gmail.com>
2021-03-23 13:59:48 +0800

    output the process of cost function.

--- PayAndTry/ProbabilityPayAndTry.cpp
+++ PayAndTry/ProbabilityPayAndTry.cpp
@@ -610,7 +610,7 @@ bool ProbabilityPayAndTry::cost_function() {
     // and estimate the vertex through the offline sample instance
     ui inexact_vertex = bin_head[max_d];
     double now_used_k_percentage = 1.0 - double(k) / ori_k;
-
+    printf("%f %f\n", now_used_k_percentage, edgeDeleteIndex->getVertexWeight(inexact_vertex));
     if(now_used_k_percentage >= edgeDeleteIndex->getVertexWeight(inexact_vertex))
         return false;
     else return true;
